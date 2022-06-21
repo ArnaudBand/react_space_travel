4762c45d477f316df3d439d06f13fffa95be78a0
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-21 12:19:03 +0500

    Create Navbar & give styling to it

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,19 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import routes from '../routes';
 
 export default function Header() {
-  return <h1>Hello Header</h1>
+  return (
+    <header>
+      <img src="../images/logo.png" alt="Logo" className="header--img" />
+      <h1 className="header--title"> Space Travelers Hub</h1>
+      <ul className="header--project">
+        {routes.map((route) => (
+          <li key={route.id}>
+            <a href={route.path}>{route.title}</a>
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
 }
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+const routes = [
+  {
+    id: 0,
+    path: '/',
+    title: 'Rockets',
+  },
+  {
+    id: 1,
+    path: '/missions',
+    title: 'Missions',
+  },
+  {
+    id: 2,
+    path: '/profile',
+    title: 'MyProfile',
+  },
+];
+
+export default routes;
