fd49f8d93bbb5c384fcd69ce3af796f34fdb0677
Arnaud <bandonkeyea@gmail.com>
2022-06-21 13:14:49 -0600

    Add propTypes and elements

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import Header from '../Header';
+import PropTypes from 'prop-types';
 
-const Rockets = () => (
+const Rockets = ({
+  id, name, description, image,
+}) => (
   <>
-    <Header />
-    <h1>Hello World!</h1>
+    <div className="block_rocket">
+      <img src={image} alt={name} width="250" className="img_rockets" />
+      <div className="text_block">
+        <h2>{name}</h2>
+        <p>{description}</p>
+        <button id={id} type="button">Reserve Rocket</button>
+      </div>
+    </div>
   </>
 );
+
+Rockets.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+};
 
 export default Rockets;
