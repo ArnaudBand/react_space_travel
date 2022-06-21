bfcb69927f15de38caf16f88e21c067483f34a3c
Arnaud <bandonkeyea@gmail.com>
2022-06-21 13:22:22 -0600

    Add header components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 import Missions from './components/Missions/Missions';
-import Rockets from './components/Rockets/Rockets';
+import RocketsPage from './pages/rockets';
 
 export default function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
-        <Route path="/" element={<Rockets />} />
+        <Route path="/" element={<RocketsPage />} />
         <Route path="/missions" element={<Missions />} />
       </Routes>
     </div>
