6a718f2a985902639bb7a09a38859d8530464115
Arnaud <bandonkeyea@gmail.com>
2022-06-21 16:21:52 -0600

    Add functionnality button

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { reserveRocket } from '../redux/rockets/rockets';
 
 const Rockets = ({
-  id, name, description, image,
-}) => (
-  <>
+  id, name, description, image, reserved,
+}) => {
+  const dispatch = useDispatch();
+
+  const revesvedBook = () => {
+    dispatch(reserveRocket(id));
+  };
+
+  return (
     <div className="block_rocket">
       <img src={image} alt={name} width="250" className="img_rockets" />
       <div className="text_block">
         <h2>{name}</h2>
-        <p>{description}</p>
-        <button id={id} type="button">Reserve Rocket</button>
+        <p>
+          {reserved && <span className="span">Reserved</span>}
+          {description}
+        </p>
+        {!reserved && (
+          <button type="button" id={id} className="reserve-btn" onClick={revesvedBook}>
+            Reserve Rocket
+          </button>
+        )}
       </div>
     </div>
-  </>
-);
+  );
+};
 
 Rockets.propTypes = {
   id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
+  reserved: PropTypes.bool.isRequired,
 };
 
 export default Rockets;
