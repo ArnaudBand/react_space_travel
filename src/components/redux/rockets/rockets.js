f17ef1c79bb981b84b7fc6b4b729b7ba11df3c01
Arnaud <bandonkeyea@gmail.com>
2022-06-21 16:23:43 -0600

    Add action-type, action-creator, reducer for reserve

--- src/components/redux/rockets/rockets.js
+++ src/components/redux/rockets/rockets.js
@@ -2,12 +2,18 @@ import getDataRockets from '../../../dataApi/rocketsApi';
 
 // // Action-types
 const GETROCKETS = 'GETROCKETS';
+const RESERVED = 'RESERVED';
 
 // Action-creator.
 export const getRocktes = () => async (dispatch) => {
   const data = await getDataRockets();
   dispatch({ type: GETROCKETS, payload: data });
 };
+
+export const reserveRocket = (id) => ({
+  type: RESERVED,
+  id,
+});
 
 const initialState = [];
 
@@ -16,6 +22,13 @@ const reducerRockets = (state = initialState, action = {}) => {
   switch (action.type) {
     case GETROCKETS:
       return action.payload;
+    case RESERVED:
+      return state.map((rocket) => {
+        if (rocket.id !== action.id) {
+          return rocket;
+        }
+        return { ...rocket, reserved: true };
+      });
     default:
       return state;
   }
