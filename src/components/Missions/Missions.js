07010c1777bbb5ce90132bb21cbe703b60049a4d
Arnaud <bandonkeyea@gmail.com>
2022-06-21 13:16:03 -0600

    Remove Header components in mission

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Header from '../Header';
 
 export default function Missions() {
   return (
     <div>
-      <Header />
       <h1>Hello Mission Component</h1>
     </div>
   );
