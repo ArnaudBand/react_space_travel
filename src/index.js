a4c28b87fe95c336bc2197117461743e44c8aab5
Arnaud <bandonkeyea@gmail.com>
2022-06-21 08:40:04 -0600

    dispatch getRocktes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from './components/redux/configureStore';
 import App from './App';
+import { getRocktes } from './components/redux/rockets/rockets';
+
+store.dispatch(getRocktes());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
 );
