9ef5846da9bdd3a398f3d7ec6206ab53f00ab9b5
Arnaud <bandonkeyea@gmail.com>
2022-06-21 13:21:45 -0600

    Import Provider and index.css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux/es/exports';
 import store from './components/redux/configureStore';
 import App from './App';
 import { getRocktes } from './components/redux/rockets/rockets';
 import { getMissions } from './components/redux/missions/missions';
+import './index.css';
 
 store.dispatch(getRocktes());
 store.dispatch(getMissions());
@@ -12,8 +14,10 @@ store.dispatch(getMissions());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
 );
